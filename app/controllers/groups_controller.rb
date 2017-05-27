da9772e152e1006ce297e101ebc87e39ee58859f
hixxxxxhi <hixxxxxhi@gmail.com>
2017-05-27 11:40:13 +0900

    Edit groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,17 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def new
-    
+    @group = Group.new
+  end
+
+  def create
+    Group.create(group_params)
+    redirect_to controller: :messages, action: :index
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, {user_ids: []})
   end
 end
