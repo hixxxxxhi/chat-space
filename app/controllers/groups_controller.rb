fd62b45cba540cc18e1da143f701794e7bf95d21
hixxxxxhi <hixxxxxhi@gmail.com>
2017-05-27 17:50:38 +0900

    Define update action in groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,15 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: "グループが編集されました。"
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, {user_ids: []})
