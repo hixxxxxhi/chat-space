6521179edbd7c2edf6bd2c054519b61b6d0cf33a
hixxxxxhi <hixxxxxhi@gmail.com>
2017-05-27 16:38:05 +0900

    Define edit action in groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,10 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, {user_ids: []})
