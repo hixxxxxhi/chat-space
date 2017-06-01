a946500ac38bd6c66c2712fb2c26d5cd045ac624
hixxxxxhi <hixxxxxhi@gmail.com>
2017-06-01 22:38:38 +0900

    Modify code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,12 +10,12 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @group = Group.find(params[:group_id])
+    group = Group.find(params[:group_id])
     @message = Message.new(body: message_params[:body], group_id: params[:group_id], user_id: current_user.id)
     if @message.save
-      redirect_to group_messages_path(params[:group_id])
+      redirect_to group_messages_path(group)
     else
-      redirect_to group_messages_path(params[:group_id]), alert: "メッセージを入力してください。"
+      redirect_to group_messages_path(group), alert: "メッセージを入力してください。"
     end
   end
 
