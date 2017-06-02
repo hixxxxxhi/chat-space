0faf24bc03224b1f4438ebb5aff41bd37efb1a73
hixxxxxhi <hixxxxxhi@gmail.com>
2017-06-02 23:18:20 +0900

    Add validation for presence of either body or image

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates :body, presence: true
+  validates :body,  presence: true, if: -> { image.blank? }
+  validates :image, presence: true, if: -> { body.blank? }
+
   mount_uploader :image, ImageUploader
 end
