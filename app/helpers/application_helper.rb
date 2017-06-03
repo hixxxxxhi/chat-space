8db1490db21c5b5989bc60f33b3b1837570e1253
hixxxxxhi <hixxxxxhi@gmail.com>
2017-06-03 15:11:10 +0900

    Refactor code for the main view by defining helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def comma_delimted_usernames(users)
+    usernames = []
+    users.each do |u|
+      usernames << u.name
+    end
+    usernames.join(", ")
+  end
+
+  def latest_message_or_status_message(group)
+    if group.messages.any?
+      if group.messages.last.body.empty?
+        "画像が投稿されています。"
+      else
+        group.messages.last.body
+      end
+    else
+      "まだメッセージはありません。"
+    end
+  end
 end
