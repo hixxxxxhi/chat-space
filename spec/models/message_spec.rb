33c374dbd3579285a9206114ec6309ba63637ab4
hixxxxxhi <hixxxxxhi@gmail.com>
2017-06-10 17:26:52 +0900

    Write test code for abnormal scenarios

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -20,6 +20,23 @@ describe Message do
     end
 
     context 'abnormal scenarios where a message cannot be saved' do
+      it "is invalid without a body or image" do
+        message = build(:message, body: nil, image: nil)
+        message.valid?
+        expect(message.errors[:body]).to include("を入力してください")
+      end
+
+      it "is invalid without a group_id" do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group_id]).to include("を入力してください")
+      end
+
+      it "is invalid without a user_id" do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user_id]).to include("を入力してください")
+      end
     end
   end
 end
