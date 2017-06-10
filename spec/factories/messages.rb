76f00926ee67ae7036512525074dc36b8528b69c
hixxxxxhi <hixxxxxhi@gmail.com>
2017-06-10 17:46:18 +0900

    Generate dummy data with faker

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,12 +1,13 @@
 include ActionDispatch::TestProcess
+require "faker"
 
 FactoryGirl.define do
 
   factory :message do
-    body      "Hello"
+    body      Faker::Lorem.sentence
     image     File.open(Rails.root.join("spec/factories/images/r2d2.png"))
-    group_id  36
-    user_id   1
+    group_id  Faker::Number.between(1, 100)
+    user_id   Faker::Number.between(1, 100)
   end
 
 end
