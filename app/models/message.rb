dc76a7c4d52b610e30b93d31accdc76751d0ddf6
hixxxxxhi <hixxxxxhi@gmail.com>
2017-06-10 17:26:36 +0900

    Add validation for group_id and user_id

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,8 @@ class Message < ApplicationRecord
 
   validates :body,  presence: true, if: -> { image.blank? }
   validates :image, presence: true, if: -> { body.blank? }
+  validates :group_id, presence: true
+  validates :user_id, presence: true
 
   mount_uploader :image, ImageUploader
 end
