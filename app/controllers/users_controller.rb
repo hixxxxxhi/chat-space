794ff8c2d5d9dbbe71eed6507fe01a137f909e3f
hixxxxxhi <hixxxxxhi@gmail.com>
2017-07-12 10:53:32 +0900

    Define users#search action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  def search
+    respond_to do |format|
+      @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not("id = ?", current_user)
+      format.json
+    end
+  end
 end
